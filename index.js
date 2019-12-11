a0e8a193b2483b359b109651310f2600fd17ef62
Allen Shin <geeuho@gmail.com>
2019-12-11 15:46:13 -0800

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
+  console.log(json)
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
@@ -10,6 +13,18 @@ function renderBooks(json) {
     main.appendChild(h2)
   })
 }
+
+function renderPages (json) {
+  const main = document.querySelector('main')
+  let total = 0
+  json.forEach(book => {
+    total += book.numberOfPages
+  })
+  const h2 = document.createElement('h2')
+  h2.innerHTML = `<h2>${total}`
+  main.appendChild(h2)
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
